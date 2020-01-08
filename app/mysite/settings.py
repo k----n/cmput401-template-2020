eb58ab5eb1a0e65e06f559c9048433afbd4f6144
k----n <kalvin.eng@ualberta.ca>
2020-01-08 08:39:08 +0000

    Local dev compose

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('MYSITE_SECRET_KEY', '')
+SECRET_KEY = os.getenv('MYSITE_SECRET_KEY', 'CHANGETHIS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ast.literal_eval(os.getenv('DEBUG', 'True'))
